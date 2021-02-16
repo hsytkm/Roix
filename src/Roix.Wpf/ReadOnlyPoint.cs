a899114fd374cc1217bd39236812112816a8dfe9
hsytkm <t_hosoya1214@yahoo.co.jp>
2021-02-16 14:09:37 +0900

    add ReadOnly structs

---
+++ src/Roix.Wpf/ReadOnlyPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Roix.Wpf
+{
+    public readonly struct ReadOnlyPoint : IEquatable<ReadOnlyPoint>
+    {
+        public readonly double X;
+        public readonly double Y;
+
+        public ReadOnlyPoint(double x, double y) => (X, Y) = (x, y);
+
+        public void Deconstruct(out double x, out double y) => (x, y) = (X, Y);
+
+        public static implicit operator ReadOnlyPoint(System.Windows.Point p) => new(p.X, p.Y);
+        public static implicit operator System.Windows.Point(in ReadOnlyPoint p) => new(p.X, p.Y);
+
+        public bool Equals(ReadOnlyPoint other) => (X, Y) == (other.X, other.Y);
+        public override bool Equals(object? obj) => (obj is ReadOnlyPoint other) && Equals(other);
+        public override int GetHashCode() => HashCode.Combine(X, Y);
+        public static bool operator ==(in ReadOnlyPoint left, in ReadOnlyPoint right) => left.Equals(right);
+        public static bool operator !=(in ReadOnlyPoint left, in ReadOnlyPoint right) => !(left == right);
+        public override string ToString() => $"{nameof(ReadOnlyPoint)} {{ {nameof(X)} = {X}, {nameof(Y)} = {Y} }}";
+    }
+}
---
+++ src/Roix.Wpf/ReadOnlyVector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Roix.Wpf
+{
+    public readonly struct ReadOnlyVector : IEquatable<ReadOnlyVector>
+    {
+        public readonly double X;
+        public readonly double Y;
+
+        public ReadOnlyVector(double x, double y) => (X, Y) = (x, y);
+
+        public void Deconstruct(out double x, out double y) => (x, y) = (X, Y);
+
+        public static implicit operator ReadOnlyVector(System.Windows.Vector s) => new(s.X, s.Y);
+        public static implicit operator System.Windows.Vector(in ReadOnlyVector s) => new(s.X, s.Y);
+
+        public bool Equals(ReadOnlyVector other) => (X, Y) == (other.X, other.Y);
+        public override bool Equals(object? obj) => (obj is ReadOnlyVector other) && Equals(other);
+        public override int GetHashCode() => HashCode.Combine(X, Y);
+        public static bool operator ==(in ReadOnlyVector left, in ReadOnlyVector right) => left.Equals(right);
+        public static bool operator !=(in ReadOnlyVector left, in ReadOnlyVector right) => !(left == right);
+        public override string ToString() => $"{nameof(ReadOnlyVector)} {{ {nameof(X)} = {X}, {nameof(Y)} = {Y} }}";
+    }
+}
